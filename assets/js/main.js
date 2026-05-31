16c2e61081ae4101551a19597cb86556728f934e
Ade Fikri <fikriade257@gmail.com>
2026-05-31 09:38:03 +0700

    feat: integrate lucide icons into summary cards and dashboard UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 $(function () {
   LoadingComponent.show("Menyiapkan dashboard...");
+  
   ModalComponent.init();
   CreateHandler.init();
   ReadHandler.init();
   UpdateHandler.init();
   DeleteHandler.init();
+
+  const renderLucideIcons = () => {
+    if (typeof window.lucide !== 'undefined' && typeof window.lucide.createIcons === 'function') {
+      window.lucide.createIcons();
+    }
+  };
+
+  renderLucideIcons();
+  window.addEventListener("load", renderLucideIcons, { once: true });
 
   requestAnimationFrame(() => {
     LoadingComponent.hide();
